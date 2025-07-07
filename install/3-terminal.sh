f36ba978fb33ddd40a184c0f3b033c239b490494
Norman Lorrain <normanlorrain@gmail.com>
2025-07-07 14:34:01 -0600

    tldr conflicts with tealdeer

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -2,5 +2,5 @@ yay -S --noconfirm --needed \
   wget curl unzip inetutils \
   fd eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
-  man tldr less whois plocate \
+  man less whois plocate \
   alacritty bash-completion
